64d773f1786dc89958cb3d603d56954eb9c86972
Stasrikan <44445171+Stasrikan@users.noreply.github.com>
2020-06-14 13:54:48 +0300

    Add files via upload

---
+++ discord-statuses/precache-manifest.9f0d2ebcb40254da872b1abe8a3d4c58.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9b9b4ea40adc150f27b81a0d0106fd6",
+    "url": "./index.html"
+  },
+  {
+    "revision": "895b4a00b272d6b38798",
+    "url": "./static/css/main.9d36e321.chunk.css"
+  },
+  {
+    "revision": "457a993d4d76c01fad6f",
+    "url": "./static/js/2.b58fcbdb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.b58fcbdb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "895b4a00b272d6b38798",
+    "url": "./static/js/main.06c49381.chunk.js"
+  },
+  {
+    "revision": "59b77ab7a6bf0fe26a8e",
+    "url": "./static/js/runtime-main.c2b37d53.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
