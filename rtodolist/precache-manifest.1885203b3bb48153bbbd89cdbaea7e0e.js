13c749cc6111f99b4009efdac3b50bbd4e8e0ac5
Stasrikan <44445171+Stasrikan@users.noreply.github.com>
2020-07-05 15:05:05 +0300

    Add files via upload

---
+++ rtodolist/precache-manifest.1885203b3bb48153bbbd89cdbaea7e0e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76040bd4b3090836ea67519a5088537a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "afd1d0209fbe43db4690",
+    "url": "./static/css/main.988352b6.chunk.css"
+  },
+  {
+    "revision": "2b82a9cd7576c2e8466c",
+    "url": "./static/js/2.c51800e2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.c51800e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "afd1d0209fbe43db4690",
+    "url": "./static/js/main.d7cf7d78.chunk.js"
+  },
+  {
+    "revision": "37807aa12135f3f0df06",
+    "url": "./static/js/runtime-main.f6a946e7.js"
+  },
+  {
+    "revision": "87e8dd41afe6a2c87cf542171d78d14f",
+    "url": "./static/media/delete.87e8dd41.svg"
+  }
+]);
